efe0a64d6e616b433ad6e9f5aed5170031bea402
Abdessattar <ghabi.1602@gmail.com>
2024-07-07 13:20:43 +0000

    Update

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """function to_kv definition"""
+from typing import Tuple
 
 
-def to_kv(k: str, v: float) -> tuple:
+def to_kv(k: str, v: float) -> Tuple[str, float]:
     """returns a tuple"""
     return (k, v * v)
