dfc91a67da0b9f6fd0c102a85df4f7cd1c7c1789
Abdessattar <ghabi.1602@gmail.com>
2024-07-07 13:24:59 +0000

    Update

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -3,6 +3,6 @@
 from typing import Tuple, Union
 
 
-def to_kv(k: str, v: Union[float, int]) -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """returns a tuple"""
     return (k, v * v)
